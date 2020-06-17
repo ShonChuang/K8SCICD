f8bd7f98e13eb7a375ff37c156c312bc4278a6b8
Shon <shon.chuang@telexpress.com>
2020-06-17 15:50:36 +0800

    測試專案

---
+++ UnitTestProject1/Controllers/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication1.Controllers.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            //Assert.Fail();
+            Assert.IsTrue(true);
+        }
+    }
+}
